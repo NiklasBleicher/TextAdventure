a59fc6f615225122e9d68a891bbf120f346fd62e
Niklas <niklas.bleicher@hs-furtwangen.de>
2020-02-08 04:04:35 +0100

    Finished XML-Parser for Doors of every Room

--- HarveysEscape/Classes/Door.cs
+++ HarveysEscape/Classes/Door.cs
@@ -6,15 +6,11 @@ namespace HarveysEscape
     {
         public bool IsOpen;
         public string LeadsTo;
-        public Item OpenedBy;
-        public string Description;
 
-        public Door(bool _IsOpen, string _LeadsTo, Item _OpenedBy, string _Description)
+        public Door(bool _IsOpen, string _LeadsTo)
         {
             this.IsOpen = _IsOpen;
             this.LeadsTo = _LeadsTo;
-            this.OpenedBy = _OpenedBy;
-            this.Description = _Description;
         }
 
     }
--- HarveysEscape/Classes/Program.cs
+++ HarveysEscape/Classes/Program.cs
@@ -27,30 +27,54 @@ namespace HarveysEscape
             
             if (input == "1")
             {
-                
-                
-                
                 XmlDocument doc = new XmlDocument();
                 doc.Load(@"../../GameBuild/Content.xml");
-                
-                //Load All Room Data: NPCs, Items, ...
-                List<Room> rooms = new List<Room>() {}; //Create Empty List of Rooms
+
+                //Get all Room Data from XML
+                List<Room> rooms = new List<Room>(); //Create Empty List of Rooms
                 XmlNodeList xmlRooms = doc.GetElementsByTagName("Value"); 
                 XmlNodeList xmlDescription = doc.GetElementsByTagName("Description");
+                
+                //Get all Door Data from XML
+                XmlNodeList xmlDoors = doc.GetElementsByTagName("Doors");
                 XmlNodeList xmlNorthDoor = doc.GetElementsByTagName("NorthDoor");
                 XmlNodeList xmlSouthDoor = doc.GetElementsByTagName("SouthDoor");
                 XmlNodeList xmlWestDoor = doc.GetElementsByTagName("WestDoor");
                 XmlNodeList xmlEastDoor = doc.GetElementsByTagName("EastDoor");
+                
+                //Get all NPC Data from XML
+                
                 for(int i = 0; i < xmlRooms.Count; i++)
                 {
                     //Get Data for Room
                     string name = (xmlRooms[i].InnerXml);
                     string description = xmlDescription[i].InnerXml;
-                    
-                    //Get Data for Doors in Room
 
+                    //Get Data for Doors in Room
+                    Door northDoor = new Door(Convert.ToBoolean(xmlNorthDoor[i].FirstChild.InnerText), xmlNorthDoor[i].LastChild.InnerText);
+                    Door southDoor = new Door(Convert.ToBoolean(xmlSouthDoor[i].FirstChild.InnerText), xmlSouthDoor[i].LastChild.InnerText);
+                    Door westDoor = new Door(Convert.ToBoolean(xmlWestDoor[i].FirstChild.InnerText), xmlWestDoor[i].LastChild.InnerText);
+                    Door eastDoor = new Door(Convert.ToBoolean(xmlEastDoor[i].FirstChild.InnerText), xmlEastDoor[i].LastChild.InnerText);
+                    
+                    //Get Data for Items in Room
+                    List<Item> items = new List<Item>();
+
+
+                    
                     //Get Data for NPC in Room
-                    //rooms.Add(new Room(name, description, northdoor, ));
+                    List<NPC> npcs = new List<NPC>();
+                    
+                    foreach (XmlNode child in xmlRooms[i].ChildNodes)
+                    {
+                        if (child.Name == "NPC")
+                        {
+                            string npcName = child
+                            NPC npc = new NPC(npcName);
+                            npcs.Add(npc);
+                        }
+                    }
+                    
+                    //rooms.Add(new Room(name, description, northDoor, southDoor, westDoor, eastDoor, items, npcs));
                 }
 
                 Console.WriteLine(rooms);
@@ -60,6 +84,8 @@ namespace HarveysEscape
                 
                 //Filter Player Elements fromXML
                 Player player = new Player();
+                XmlDocument pla = new XmlDocument();
+                doc.Load(@"../../GameBuild/Player.xml");
                 
                 
                 TA = new TextAdventure(rooms, player, currentRoom);
